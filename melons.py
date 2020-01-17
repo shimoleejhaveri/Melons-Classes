4ed1f0eeece073b34f561792ce479b95e0afe88c
Your Name <you@example.com>
2020-01-17 13:05:11 -0800

    Part 2 done

--- melons.py
+++ melons.py
@@ -1,49 +1,24 @@
 """Classes for melon orders."""
 
 
-class DomesticMelonOrder():
-    """A melon order within the USA."""
+class AbstractMelonOrder():
+    """Abstract Melon Order Class"""
 
-    def __init__(self, species, qty):
-        """Initialize melon order attributes."""
-
+    def __init__(self, species, qty, country_code='USA'):
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder():
-    """An international (non-US) melon order."""
-
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
 
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        if self.species == "Christmas melon":
+            base_price = 7.5
+
+        else:
+            base_price = 5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -57,3 +32,24 @@ class InternationalMelonOrder():
         """Return the country code."""
 
         return self.country_code
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    order_type = "international"
+    tax = 0.17
+
+    def get_total(self):
+        
+        total = super().get_total()
+
+        if self.qty < 10:
+            total += 3
+
+        return total
